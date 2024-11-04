7e89f3d88a62e1444aceed151c154e5623f1d524
Soubhagyadhal2004 <soubhagyadhal12345@gmail.com>
2024-11-04 13:20:31 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -68,7 +68,7 @@ $('.slider_btn_next').on('click', function (e) {
 
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(16.509400, 80.625040),
+        center: new google.maps.LatLng(16.491977, 80.500648),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
